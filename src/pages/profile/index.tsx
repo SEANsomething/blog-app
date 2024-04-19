5836dbd330c5fc62e44fdee891ee00c75a2ac543
SangWon Chu <sangwonchu@SangWonui-MacBookPro.local>
2024-04-19 18:58:16 +0900

    post list styling

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,3 +1,13 @@
+import Header from "../../components/Header";
+import Profile from "../../components/Profile";
+import Footer from "../../components/Footer";
+import PostList from "../../components/PostList";
+
 export default function ProfilePage() {
-  return <h1>Profile Page</h1>;
+  return <>
+		<Header />
+		<Profile />
+		<PostList hasNavigation={false}/>
+		<Footer />
+	</>;
 }
